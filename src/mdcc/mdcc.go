0229e27ca1292dad83bee7af7dcaea018ab6501b
Mike Lewis <mikelikespie@gmail.com>
2012-06-30 16:22:44 -0700

    Adding gitignore and mdcc

---
+++ src/mdcc/mdcc.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+)
+
+type Tx interface {
+	Timeout() int64;
+
+	DoExecute();
+	DoOnFailure();
+	DoOnAccept();
+	DoOnCommit(success bool);
+	DoFinally(success bool, timeout bool);
+	DoFinallyRemote(success bool, timeout bool);
+}
+
+type Txn struct {
+	Execute func();
+	OnFailure func();
+	OnAccept func();
+	OnCommit func(success bool);
+	Finally func(success bool, timeout bool);
+	FinallyRemote func(success bool, timeout bool);
+}
+
+func main () {
+}
